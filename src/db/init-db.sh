66e9a87163b6a3fceab7e78b53bb2bdcedc0f8c4
syedhassaanahmed <hassaan.brix@gmail.com>
2024-10-31 13:00:05 +0100

    Fix SqlCmd issues in the test

--- src/db/init-db.sh
+++ src/db/init-db.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+SQLCMD="/opt/mssql-tools18/bin/sqlcmd"
+
 #do this in a loop because the timing for when the SQL instance is ready is indeterminate
 for i in {1..60};
 do
-    /opt/mssql-tools/bin/sqlcmd -S "$MSSQL_HOST" -U "$MSSQL_SA_USER" -P "$MSSQL_SA_PASSWORD"
+    $SQLCMD -C -S "$MSSQL_HOST" -U "$MSSQL_SA_USER" -P "$MSSQL_SA_PASSWORD"
 
     if [ $? -eq 0 ]
     then
@@ -15,5 +17,5 @@ do
     fi
 done
 
-/opt/mssql-tools/bin/sqlcmd -S "$MSSQL_HOST" -U "$MSSQL_SA_USER" -P "$MSSQL_SA_PASSWORD" -i create_db.sql -v DBName="$DB_NAME"
-/opt/mssql-tools/bin/sqlcmd -S "$MSSQL_HOST" -U "$MSSQL_SA_USER" -P "$MSSQL_SA_PASSWORD" -d "$DB_NAME" -i create_table.sql
+$SQLCMD -C -S "$MSSQL_HOST" -U "$MSSQL_SA_USER" -P "$MSSQL_SA_PASSWORD" -i create_db.sql -v DBName="$DB_NAME"
+$SQLCMD -C -S "$MSSQL_HOST" -U "$MSSQL_SA_USER" -P "$MSSQL_SA_PASSWORD" -d "$DB_NAME" -i create_table.sql
